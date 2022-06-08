1d4823312382f18ed894fd7b20dc8eafaad6da25
Shamil <toshamilgis@gmail.com>
2022-06-08 23:00:00 +0300

    lst

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,11 +2,11 @@ import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
-    site: "https://next-app-example-bay.vercel.app/",
+    site: process.env.NEXTAUTH_URL,
     providers: [
         GitHubProvider({
-            clientId: "d10baf2fe9a03be8d42a",
-            clientSecret: "e95ce327dba774f5e5249ed6eb2f292d20389345"
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
         })
     ],
     secret: process.env.SECRET
